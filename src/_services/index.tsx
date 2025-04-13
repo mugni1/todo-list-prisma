9730034c2dbd923aba828a2db123fe48b5c49c11
mugni1 <asepabdulmugni22@gmail.com>
2025-04-13 09:45:48 +0700

    modified

--- src/_services/index.tsx
+++ src/_services/index.tsx
@@ -1,8 +1,10 @@
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL_APP;
+
 export async function getTodos(status?: string) {
   const fetchURL =
     status == "done"
-      ? "http://localhost:3000/api/todos?status=done"
-      : "http://localhost:3000/api/todos";
+      ? baseURL + "/api/todos?status=done"
+      : baseURL + "/api/todos";
   const res = await fetch(fetchURL);
   return res.json();
 }
